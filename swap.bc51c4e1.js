4c8cb7a3dde32eb30197e4b027c2c6fc20108aea
muhuahaha <kimyou2019@gmail.com>
2020-06-26 22:39:37 +0200

    ada

--- swap.bc51c4e1.js
+++ swap.bc51c4e1.js
@@ -117,236 +117,59 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"js/bro.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.bro = void 0;
-
-var bro = function bro(greeting) {
-  return "".concat(greeting, ", bro");
-};
-
-exports.bro = bro;
-},{}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"js/scroll.js":[function(require,module,exports) {
-var header = document.querySelector('.main-header');
-window.addEventListener('scroll', function () {
-  var scrollPos = window.scrollY;
-
-  if (scrollPos > 150) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
-
-function myFunction() {
-  document.getElementById('menu-btn').checked = false;
-}
-
-document.getElementById('logo-btn').addEventListener('click', function () {
-  // console.log('click');
-  document.getElementById('menu-btn').checked = false;
-});
-document.getElementById('myDIV').addEventListener('click', myFunction);
-},{}],"images/svg/ava01.svg":[function(require,module,exports) {
-module.exports = "/ava01.4d054c61.svg";
-},{}],"images/svg/ava02.svg":[function(require,module,exports) {
-module.exports = "/ava02.0b0b6f9c.svg";
-},{}],"images/svg/ava03.svg":[function(require,module,exports) {
-module.exports = "/ava03.bf8eaf42.svg";
-},{}],"images/svg/ava04.svg":[function(require,module,exports) {
-module.exports = "/ava04.1e5173c7.svg";
-},{}],"images/svg/ava05.svg":[function(require,module,exports) {
-module.exports = "/ava05.f3b70f85.svg";
-},{}],"images/svg/ava06.svg":[function(require,module,exports) {
-module.exports = "/ava06.0dbc8532.svg";
-},{}],"images/svg/ava07.svg":[function(require,module,exports) {
-module.exports = "/ava07.8425800f.svg";
-},{}],"images/svg/ava08.svg":[function(require,module,exports) {
-module.exports = "/ava08.b031a973.svg";
-},{}],"images/svg/ava09.svg":[function(require,module,exports) {
-module.exports = "/ava09.6c044392.svg";
-},{}],"images/svg/ava11.svg":[function(require,module,exports) {
-module.exports = "/ava11.2dfa909a.svg";
-},{}],"images/svg/ava12.svg":[function(require,module,exports) {
-module.exports = "/ava12.952462bf.svg";
-},{}],"images/svg/ava13.svg":[function(require,module,exports) {
-module.exports = "/ava13.15035cdc.svg";
-},{}],"js/swap.js":[function(require,module,exports) {
-"use strict";
-
-var _ava = _interopRequireDefault(require("../images/svg/ava01.svg"));
-
-var _ava2 = _interopRequireDefault(require("../images/svg/ava02.svg"));
-
-var _ava3 = _interopRequireDefault(require("../images/svg/ava03.svg"));
-
-var _ava4 = _interopRequireDefault(require("../images/svg/ava04.svg"));
-
-var _ava5 = _interopRequireDefault(require("../images/svg/ava05.svg"));
-
-var _ava6 = _interopRequireDefault(require("../images/svg/ava06.svg"));
-
-var _ava7 = _interopRequireDefault(require("../images/svg/ava07.svg"));
-
-var _ava8 = _interopRequireDefault(require("../images/svg/ava08.svg"));
-
-var _ava9 = _interopRequireDefault(require("../images/svg/ava09.svg"));
-
-var _ava10 = _interopRequireDefault(require("../images/svg/ava11.svg"));
-
-var _ava11 = _interopRequireDefault(require("../images/svg/ava12.svg"));
-
-var _ava12 = _interopRequireDefault(require("../images/svg/ava13.svg"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+})({"js/swap.js":[function(require,module,exports) {
 function randomPick(arr) {
   var selected = arr[Math.floor(Math.random() * arr.length)];
   return selected;
 }
 
-var images = [_ava2.default, _ava3.default, _ava4.default, _ava5.default, _ava6.default, _ava7.default, _ava8.default, _ava9.default, _ava10.default, _ava11.default, _ava12.default];
+var images = ['images/svg/ava02.svg', 'images/svg/ava03.svg', 'images/svg/ava04.svg', 'images/svg/ava05.svg', 'images/svg/ava06.svg', 'images/svg/ava07.svg', 'images/svg/ava08.svg', 'images/svg/ava09.svg', 'images/svg/ava11.svg', 'images/svg/ava12.svg', 'images/svg/ava13.svg'];
 var txt = ['Hallo', 'Hi', '안녕하세요', 'Hello', 'Hola', 'Moin', 'Yoo']; // function randomHover() {}
 
 var test1 = document.getElementById('hover');
-test1.addEventListener('mouseenter', function () {
+test1.addEventListener('mouseenter', function (event) {
   var myImage = document.getElementById('hover');
   var selImage = randomPick(images);
-  myImage.src = selImage;
   var selText = randomPick(txt);
   document.getElementById('yoo').innerText = "".concat(selText, "!");
+  myImage.src = selImage;
 });
 var test2 = document.getElementById('hover');
-test2.addEventListener('mouseleave', function () {
+test2.addEventListener('mouseleave', function (event) {
   var myImage = document.getElementById('hover');
-  myImage.src = _ava.default;
+  myImage.src = 'images/svg/ava01.svg';
   var selText = randomPick(txt);
   document.getElementById('yoo').innerText = "YTK ".concat(selText, "!");
-});
-},{"../images/svg/ava01.svg":"images/svg/ava01.svg","../images/svg/ava02.svg":"images/svg/ava02.svg","../images/svg/ava03.svg":"images/svg/ava03.svg","../images/svg/ava04.svg":"images/svg/ava04.svg","../images/svg/ava05.svg":"images/svg/ava05.svg","../images/svg/ava06.svg":"images/svg/ava06.svg","../images/svg/ava07.svg":"images/svg/ava07.svg","../images/svg/ava08.svg":"images/svg/ava08.svg","../images/svg/ava09.svg":"images/svg/ava09.svg","../images/svg/ava11.svg":"images/svg/ava11.svg","../images/svg/ava12.svg":"images/svg/ava12.svg","../images/svg/ava13.svg":"images/svg/ava13.svg"}],"js/rerun.js":[function(require,module,exports) {
-// This changes everything
-// retrieve the element
-var element = document.getElementById('logo'); // reset the transition by...
-
-element.addEventListener('mouseover', function (e) {
-  e.preventDefault; // -> removing the class
-
-  element.classList.remove('run-animation'); // -> triggering reflow /* The actual magic */
-  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
-  // This was, from the original tutorial, will no work in strict mode. Thanks Felis Phasma! The next uncommented line is the fix.
-  // element.offsetWidth = element.offsetWidth;
-
-  void element.offsetWidth; // -> and re-adding the class
-
-  element.classList.add('run-animation');
-}, false);
-},{}],"js/custom-gsap.js":[function(require,module,exports) {
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-ScrollTrigger.defaults({
-  toggleActions: 'restart pause resume pause'
-});
-gsap.utils.toArray('.nav-link a').forEach(function (a) {
-  a.addEventListener('click', function (e) {
-    e.preventDefault();
-    gsap.to(window, {
-      duration: 1,
-      scrollTo: e.target.getAttribute('href')
-    });
-  });
-});
-},{}],"js/index.js":[function(require,module,exports) {
-"use strict";
-
-var _bro = require("./bro");
-
-require("../styles/main.scss");
-
-require("./scroll.js");
-
-require("./swap.js");
-
-require("./rerun.js");
-
-require("./custom-gsap.js");
-
-document.querySelector('h4').textContent = (0, _bro.bro)("How's it going");
-},{"./bro":"js/bro.js","../styles/main.scss":"styles/main.scss","./scroll.js":"js/scroll.js","./swap.js":"js/swap.js","./rerun.js":"js/rerun.js","./custom-gsap.js":"js/custom-gsap.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+}); // const test = document.getElementById('hover');
+// window.addEventListener(
+//   'load',
+//   event => {
+//     const myImage = document.getElementById('hover');
+//     setTimeout(function() {
+//       const selImage = randomPick(images);
+//       myImage.src = selImage;
+//     }, 5000);
+//     setTimeout(function() {
+//       myImage.src = 'img/ava1.png';
+//     }, 7000);
+//   },
+//   false
+// );
+// test.addEventListener(
+//   'load',
+//   event => {
+//     const myImage = document.getElementById('hover');
+//     setTimeout(function() {
+//       const selImage = randomPick(images);
+//       myImage.src = selImage;
+//     }, 7000);
+//     setTimeout(function() {
+//       myImage.src = 'img/ava1.png';
+//     }, 9000);
+//   },
+//   false
+// );
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -374,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53378" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52361" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -550,5 +373,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/js.00a46daa.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/swap.js"], null)
+//# sourceMappingURL=/swap.bc51c4e1.js.map
